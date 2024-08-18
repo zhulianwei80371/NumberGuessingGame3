87835da9c26e785a3d90b4b65194ae672b8fdb05
lianweizhu <632229677@qq.com>
2024-08-18 21:34:41 +0800

    init

---
+++ src/main/java/org/example/numberguessinggame3/NumberGuessingGame3Application.java
@@ -0,0 +1,13 @@
+package org.example.numberguessinggame3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumberGuessingGame3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NumberGuessingGame3Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/numberguessinggame3/NumberGuessingGame3ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.numberguessinggame3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NumberGuessingGame3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
